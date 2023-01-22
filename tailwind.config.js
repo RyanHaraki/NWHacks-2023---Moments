105364df2c980f75da6eea70667caf907d7f179d
RyanHaraki <ryan.haraki@hotmail.com>
2023-01-21 18:01:30 -0800

    Co-authored-by: Scott Langille <scottlangille@users.noreply.github.com> Co-authored-by: Alison Hardy <alichiba@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,31 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        tilt: "tilt 5s infinite linear",
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(8deg)",
+          },
+          "75%": {
+            transform: "rotate(-8deg)",
+          },
+        },
+      },
+      colors: {
+        blue: {
+          10: "#3574f4",
+        },
+      },
+    },
   },
   plugins: [],
 };
