bd9d086630f642a8d339e46d5e11cd5da84eef66
RyanHaraki <ryan.haraki@hotmail.com>
2023-01-22 02:37:22 -0800

    Co-authored-by: Alison Hardy <alichiba@users.noreply.github.com> Co-authored-by: Scott Langille <scottlangille@users.noreply.github.com>

--- pages/inventory.js
+++ pages/inventory.js
@@ -2,16 +2,23 @@ import React from "react";
 import GalleryItem from "@/components/ui/GalleryItem";
 
 const Inventory = () => {
+  const user = localStorage.getItem("address");
+  // check to see if there is a user on page load. if not redirect to "/"
+  // render the actual wallet from localstorage
+
   return (
-    <div className="flex space-y-2 flex-col items-center justify-start h-screen bg-black">
-      <div className="flex items-end justify-start space-x-6 mt-14 w-screen overflow-visible h-fit">
-        <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-600 to-blue-10 overflow-visible bg-cover bg-repeat h-14 ml-24">
+    <div className="flex space-y-2 flex-col items-center justify-start h-screen bg-black overflow-hidden">
+      <div className="flex items-end justify-start space-x-6 mt-14 w-screen h-fit">
+        <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-600 to-blue-10 bg-cover bg-repeat h-14 ml-24">
           Inventory
         </h1>
-        <h5 className="text-2xl text-gray-500">0x wallet address</h5>
+        <h5 className="text-2xl text-gray-500"></h5>
       </div>
       <div className="bg-gradient-to-r from-fuchsia-600 to-blue-10 h-2 w-screen"></div>
-      <div>
+      <div className="grid grid-cols-1 gap-y-20 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 pt-10 w-screen ml-48">
+        <GalleryItem />
+        <GalleryItem />
+        <GalleryItem />
         <GalleryItem />
       </div>
     </div>
